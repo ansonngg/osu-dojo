c058e0fb28777d647f430697dae714dfed730bb6
ansonngg <ngkawai9886@gmail.com>
2026-06-19 14:45:40 -0400

    Rebuild the frontend project with the latest pnpm

--- OsuDojo.Web/ClientApp/next.config.ts
+++ OsuDojo.Web/ClientApp/next.config.ts
@@ -1,20 +1,9 @@
 import type { NextConfig } from "next";
 
-const apiUrl = process.env.API_URL ?? "http://localhost:5051";
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${apiUrl}/api/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
